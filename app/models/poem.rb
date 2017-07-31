2dd7565fad599d51c9ab51f0207a9b74d6afcf54
satochikin <satochikin@sakikawasatoshi-no-MacBook-Air.local>
2017-07-31 17:28:43 +0900

    poems

---
+++ app/models/poem.rb
@@ -0,0 +1,3 @@
+class Poem < ActiveResource::Base
+  self.site ="https://shrouded-dawn-56862.herokuapp.com"
+end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :poems, only: [:index, :show]
+
    root 'top#index'
 
    if Rails.env.development?
